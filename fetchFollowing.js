83e3777928484221dc0e7041e78a26c5d1cc80b9
TaYaKi71751 <shantian.tw@gmail.com>
2023-06-15 14:21:12 +0900

    Add fetchFollowing.js

---
+++ fetchFollowing.js
@@ -0,0 +1,55 @@
+// run at Breakpoint focused on `fetchFollowing`
+// Developer tools -> Network -> Filter URLs (Following) -> Stack Trace -> fetchFollowing
+(async (user_id) => {
+  const followings = [];
+  let cursor = undefined;
+  let count = 20;
+  let data_cursor = '';
+  function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  }
+  const fetchFollowing = async (user_id, cursor, count) => {
+    console.log({ user_id, cursor, count });
+    const results = await e.graphQL(
+      T(),
+      {
+        userId: user_id,
+        count,
+        cursor,
+        includePromotedContent: !1,
+        ...(0, o.d) (t)
+      },
+      D('Following')
+    );
+    console.log(results);
+    return results;
+  }
+
+  let data = undefined;
+
+  do {
+    await sleep(1000);
+    console.log(cursor);
+    try {
+      data = await fetchFollowing(user_id, cursor, count);
+    } catch (e) {
+      console.error(e.toString());
+      if (e.toString().includes("Too Many Requests")) {
+        await sleep(2500);
+      }
+    }
+    console.log(data);
+    data_cursor = data.user.result.timeline.timeline.instructions.filter((i) => (i.type == 'TimelineAddEntries'))[0].entries.filter((e) => (e.entryId.startsWith('cursor-bottom-')))[0].content.value;
+    console.log({ cursor, data_cursor });
+    if(!(data.user.result.timeline.timeline.instructions.filter((i) => (i.type == 'TimelineAddEntries'))[0].entries.filter((e) => (e.entryId.startsWith('user-'))).map((u) => (u.content.itemContent.user_results.result)).filter((u) => { if (!followings.filter((_u) => (`${_u?.rest_id}` == `${u?.rest_id}`)).length) return u; }).length)) break;
+    if (`${cursor}` == `${data_cursor}`) {
+      if (data.user.result.timeline.timeline.instructions.filter((i) => (i.type == 'TimelineTerminateTimeline')).length) break;
+      count--;
+      if (count > 0) continue;
+      break;
+    }
+    cursor = data_cursor;
+    data.user.result.timeline.timeline.instructions.filter((i) => (i.type == 'TimelineAddEntries'))[0].entries.filter((e) => (e.entryId.startsWith('user-'))).map((u) => (u.content.itemContent.user_results.result)).forEach((u) => { if (!followings.filter((_u) => (`${_u?.rest_id}` == `${u?.rest_id}`)).length) followings.push(u) });
+  } while (typeof cursor != 'undefined')
+  console.log(followings);
+})('USER_ID_HERE')
