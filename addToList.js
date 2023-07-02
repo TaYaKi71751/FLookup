effce3c6fd683b6ffbdc068f36dc59a6de3ccba4
TaYaKi71751 <56256195+TaYaKi71751@users.noreply.github.com>
2023-07-02 10:25:46 +0900

    Add list_id to  addToList.js

--- addToList.js
+++ addToList.js
@@ -1,25 +1,25 @@
 // run at Breakpoint focused on `addToList`
 // Developer tools -> Network -> Filter URLs (ListAddMember) -> Stack Trace -> addToList
-(async (users) => {
+(async (users,list_id) => {
   for(let i = 0;i < users.length;i++){
     const user = users[i];
     function sleep(milliseconds) {
       return new Promise(resolve => setTimeout(resolve, milliseconds));
     }
-    async function addToList(user){
+    async function addToList(user,list_id){
       try {
         await e.graphQL(c(), {
-            listId: a,
+            listId: list_id,
             userId: user.rest_id,
             ...(0, s.S) (t)
           }, ke).then((e => (0, i.Fv) (e.list, o.Z)))
       } catch (e){
         await sleep(1500);
-        await addToList(user);
+        await addToList(user,list_id);
       }
     }
-    if(user){
-      await addToList(user);
+    if(user && list_id){
+      await addToList(user,list_id);
     }else break;
   }
-})('USERS_ARRAY_HERE')
+})('USERS_ARRAY_HERE','LIST_ID_HERE')
